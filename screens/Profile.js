6ac9eea138fe65771dc3b12a0d5e8dcd2fb43af4
sanjaykumar07 <s.sanjaykumar2007@gmail.com>
2021-08-15 17:38:18 +0530

    stage1

---
+++ screens/Profile.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+
+export default class ProfileScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>Profile Screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
